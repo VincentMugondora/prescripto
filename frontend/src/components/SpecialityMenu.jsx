5701eae93df3a3e19879d4bc21d03eafd3df7327
VincentMugondora <vinmugondora@gmail.com>
2024-11-08 14:55:36 +0200

    "Updated App.jsx, SpecialityMenu.jsx, and Home.jsx components, and added grid template column configuration to tailwind.config.js"

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div className="flex flex-col items-center gap-4 py-1 text-gray-800" id="speciality">
+    <div className="flex flex-col mt-[50px] items-center gap-4 py-1 text-gray-800" id="speciality">
       <h1 className="text-3xl font font-medium">Find By Speciality</h1>
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors, schedule
