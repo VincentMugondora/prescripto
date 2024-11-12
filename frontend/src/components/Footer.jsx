ad9f8506bf29b8d887b8b100876b664419a43fb7
VincentMugondora <vinmugondora@gmail.com>
2024-11-12 13:56:59 +0200

    "Updated Footer component with minor layout and styling changes"

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,13 +2,12 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="md:mx-10"> 
+    <div className="md:mx-10">
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-
         {/* ------- Left Section ------- */}
         <div>
           <img className="mb-5 w-40" src={assets.logo} alt="" />
-          <p>
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -17,9 +16,9 @@ const Footer = () => {
         </div>
 
         {/* ------- Center Section ------- */}
-        <div>
-          <p>COMPANY</p>
-          <ul>
+        <div className="">
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About Us</li>
             <li> Contact Us</li>
@@ -29,8 +28,8 @@ const Footer = () => {
 
         {/* ------- Right Section ------- */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+263 777 530 322</li>
             <li>vinmugondora@gmail.com</li>
           </ul>
@@ -40,7 +39,7 @@ const Footer = () => {
       {/* ------- Copyright Text ------- */}
       <div>
         <hr />
-        <p>Copyright &copy; 2024 Prescripto. All rights reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright &copy; 2024 Prescripto. All rights reserved.</p>
       </div>
     </div>
   );
