7be3d132f62d98503ceb7d140d6847f0bc0515fe
VincentMugondora <vinmugondora@gmail.com>
2024-11-14 09:55:36 +0200

    "Updated Navbar component to add navigation on logo click, added font imports and styling to index.css, and modified Doctors page to use AppContext and useParams"

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from "../context/AppContext";
 
 const Doctors = () => {
+
+  const { speciality } = useParams()
+  
+  const {doctors} = useContext(AppContext)
+
   return (
     <div>
-      
+      <p>Browse Through the doctors specialist.</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+          {
+            
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Doctors
